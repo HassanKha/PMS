47e3ee3889bab121bf6fc038f44dd1c00d4a28f4
Hassan Khaled <58123330+HassanKha@users.noreply.github.com>
2025-06-14 19:53:06 +0300

    Packges Installed

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-pro-sidebar/dist/css/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
