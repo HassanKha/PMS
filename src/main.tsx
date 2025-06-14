ff0f5b9d85b18de2ad17bb72c31478520c166a31
Hassan Khaled <58123330+HassanKha@users.noreply.github.com>
2025-06-14 19:57:56 +0300

    React-router-dom has been installed

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
-import 'react-pro-sidebar/dist/css/styles.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
