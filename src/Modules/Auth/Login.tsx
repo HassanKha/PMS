322c320b58ecaf80848fb82e268f42ca026d40d9
Hossam-Elshal <hossamelshal@std.mans.edu.eg>
2025-06-17 20:07:05 +0300

    login spinner

--- src/Modules/Auth/Login.tsx
+++ src/Modules/Auth/Login.tsx
@@ -1,10 +1,11 @@
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../../styles/register.css";
 import PMSIcon from "../../assets/PMSIcon.png";
-import LoginBg from "../../assets//LoginBG.svg";
+import LoginBg from "../../assets/LoginBG.svg";
 import axios from "axios";
+import { useState } from "react";
 
 type LoginFormInputs = {
   email: string;
@@ -19,28 +20,31 @@ function Login() {
   } = useForm<LoginFormInputs>();
 
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const onSubmit = async (data: LoginFormInputs) => {
-  try {
-    const response = await axios.post(
-      "https://upskilling-egypt.com:3003/api/v1/Users/Login",
-      data
-    );
-    navigate("/dashboard");
-    toast.success("Login success!");
-    localStorage.setItem("token", response?.data?.token);
-  } catch (error: unknown) {
-    // Type guard to check if error is an AxiosError
-    if (axios.isAxiosError(error)) {
-      const message = error.response?.data?.message || "Login Failed!";
-      toast.error(message);
-      console.log("Login Error:", error.response?.data || error.message);
-    } else {
-      toast.error("Unexpected error");
-      console.log("Unexpected Error:", error);
+    setLoading(true); 
+    try {
+      const response = await axios.post(
+        "https://upskilling-egypt.com:3003/api/v1/Users/Login",
+        data
+      );
+      navigate("/dashboard");
+      toast.success("Login success!");
+      localStorage.setItem("token", response?.data?.token);
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        const message = error.response?.data?.message || "Login Failed!";
+        toast.error(message);
+        console.log("Login Error:", error.response?.data || error.message);
+      } else {
+        toast.error("Unexpected error");
+        console.log("Unexpected Error:", error);
+      }
+    } finally {
+      setLoading(false); 
     }
-  }
-};
+  };
 
   return (
     <div
@@ -54,7 +58,13 @@ function Login() {
       }}
     >
       <img src={PMSIcon} alt="PMSIcon" className="w-25 h-25" />
-      <div className="d-flex Verify-container g-3 flex-column align-items-center justify-content-center">
+
+      <div
+        className="Verify-container d-flex flex-column align-items-center justify-content-center px-3 w-100"
+        style={{
+          maxWidth: "500px",
+        }}
+      >
         <div className="w-100 py-3 Verify-main-container d-flex flex-column justify-content-lg-start border-0 shadow-lg">
           <div className="text-start mt-5 mx-5">
             <h2
@@ -137,12 +147,28 @@ function Login() {
                   </div>
                 )}
               </div>
+
+              <div className="links-container d-flex justify-content-between mb-3">
+                <Link
+                  to="/register"
+                  className="text-decoration-none text-white"
+                >
+                  Register Now?
+                </Link>
+                <Link
+                  to="/forget-password"
+                  className="text-decoration-none text-white"
+                >
+                  Forget Password?
+                </Link>
+              </div>
             </div>
 
             <div className="d-flex justify-content-center mx-sm-5 mx-lg-0 mt-4">
               <button
                 type="submit"
-                className="btn fw-bold w-100"
+                className="btn fw-bold w-100 d-flex align-items-center justify-content-center"
+                disabled={loading} 
                 style={{
                   maxWidth: "300px",
                   background: "linear-gradient(45deg, #ffa726, #ff9800)",
@@ -152,7 +178,16 @@ function Login() {
                   fontSize: "18px",
                 }}
               >
-                Login
+                {loading ? (
+                  <div
+                    className="spinner-border spinner-border-sm text-light"
+                    role="status"
+                  >
+                    <span className="visually-hidden">Loading...</span>
+                  </div>
+                ) : (
+                  "Login"
+                )}
               </button>
             </div>
           </form>
