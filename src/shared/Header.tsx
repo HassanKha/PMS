14bed144a048032b279c13eeba06b639a9b02fa4
Hassan Khaled <58123330+HassanKha@users.noreply.github.com>
2025-06-20 18:56:11 +0300

    Sarching & pagination & listing

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -2,24 +2,28 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import type { HeaderProps } from '../interfaces/HeaderProps'
+import { Link, useLocation } from 'react-router-dom'
 
 const Header: React.FC<HeaderProps> = ({ Title, BtnTitle
  //, onBtnClick
  }) => {
+
+  const location = useLocation()
   return (
    <div className="d-flex flex-column rounded-2 flex-lg-row w-100 font-mont justify-content-between bg-white p-3 px-lg-4 py-lg-3 align-items-start align-items-lg-center mb-4">
       <h1 className="h4 h2-lg fw-bold text-dark mb-2 mb-lg-0">{Title}</h1>
-      <button
+      <Link
         type="button"
-        className="btn text-white d-flex align-items-center gap-2 px-3 px-lg-4 py-2"
+        className="btn LinkBTM text-white d-flex align-items-center gap-2 px-3 px-lg-4 py-2"
         style={{
           border: 'none',
           borderRadius: '8px',
         }}
+        to={location.pathname == "/dashboard/projects" ? '/dashboard/project-data' : '/dashboard/tasks'}
       >
         <FontAwesomeIcon icon={faPlus} style={{ color: '#fff', fontSize: '18px' }} />
         <span>{BtnTitle}</span>
-      </button>
+      </Link>
     </div>
    
   )
