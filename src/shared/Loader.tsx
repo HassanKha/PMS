8a04bf41bf4d55ea46d55c705d2dcd465e725c92
besheir77 <bassammohamed398@gmail.com>
2025-06-21 22:16:55 +0300

    load

--- src/shared/Loader.tsx
+++ src/shared/Loader.tsx
@@ -1,13 +1,29 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons/faSpinner'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-export default function Loader() {
+interface LoaderProps {
+  name?: string;
+}
+
+
+
+export default function Loader({ name }: LoaderProps) {
   return (
     <>
-    <div className="text-center">
-        <div className="spinner-border text-success mt-4 loading text-center" role="status">
-        <span className="visually-hidden">Loading...</span>
-    </div>
-  </div>
+    <div className="rounded p-5 text-center">
+                        <FontAwesomeIcon
+                          icon={faSpinner}
+                          spin
+                          size="2x"
+                          style={{ color: "#5a8a7a" }}
+                          className="mb-3"
+                        />
+                        <h5 className="text-muted">Loading {name}...</h5>
+                        <p className="text-muted mb-0">
+                          Please wait while we fetch your data
+                        </p>
+        </div>
   </>
   )
 }
